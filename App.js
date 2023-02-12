fa1c3cc3517b57e62121c2eeaa5ac8d93507abea
Emilia Mäkelä <bgr887@myy.haaga-helia.fi>
2023-02-13 01:28:41 +0200

    2 commit

--- App.js
+++ App.js
@@ -1,20 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, TextInput, Button } from 'react-native';
+import {Picker} from '@react-native-picker/picker';
 
-export default function App() {
+const CurrencyConverter = () => {
+  const [selectedCurrency, setSelectedCurrency] = useState('');
+  const [amount, setAmount] = useState(0);
+  const [result, setResult] = useState(0);
+  const [currencies, setCurrencies] = useState([]);
+
+  useEffect(() => {
+    fetchCurrencies();
+  }, []);
+
+  const myHeaders = new Headers();
+  myHeaders.append("apikey", "JgyAIVPt8tGfx0tcg0ibMzWQarTtTuO1");
+
+  const requestOptions = {
+    method: 'GET',
+    redirect: 'follow',
+    headers: myHeaders
+  };
+
+  const fetchCurrencies = async () => {
+    const response = await fetch('https://api.apilayer.com/exchangerates_data/symbols', requestOptions);
+    const data = await response.json();
+    setCurrencies(Object.keys(data.symbols));
+  };
+  
+  const convertCurrency = async () => {
+    const response = await fetch(`https://api.apilayer.com/exchangerates_data/convert?to=EUR&from=${selectedCurrency}&amount=${amount}`, requestOptions);
+    const data = await response.json();
+    setResult(data.result);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{margin:120, marginTop:'80%'}}>
+      <Text style={{ fontSize: 20, fontWeight:'bold' }}>{result.toFixed(2)} €</Text>
+      <Picker
+        selectedValue={selectedCurrency}
+        onValueChange={value => setSelectedCurrency(value)}
+      >
+        {currencies.map(currency => <Picker.Item key={currency} label={currency} value={currency} />)}
+      </Picker>
+      <TextInput
+        keyboardType="numeric"
+        onChangeText={text => setAmount(text)}
+        value={amount}
+      />
+      <Button title="Convert" onPress={convertCurrency} />
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default CurrencyConverter;
